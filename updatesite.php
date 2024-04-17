6585747995c95116fc5f2b002576d70cee11f642
Kevin Trotter <cskevint@gmail.com>
2024-04-17 06:03:01 -0700

    added redirect

--- updatesite.php
+++ updatesite.php
@@ -1,3 +1,9 @@
 <?php
 echo exec("git pull --rebase");
 ?>
+<p>Redirecting to homepage in 5 seconds...</p>
+<script>
+window.setTimeout(function () {
+    window.location.href = "/";
+}, 5000);
+</script>
