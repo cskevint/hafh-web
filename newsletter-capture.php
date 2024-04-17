9b9d27b26931357118c5467ddba1b5304d361f5b
Kevin Trotter <cskevint@gmail.com>
2024-04-17 08:18:59 -0700

    added configuration

--- newsletter-capture.php
+++ newsletter-capture.php
@@ -16,7 +16,7 @@ if (sendMail($fromEmail, $fromName, $subject, $messageBody)) {
     $_SESSION['notice'] = 'There was an error. Please email us: <a href="mailto:selena@houndawayfromhome.com">selena@houndawayfromhome.com</a>';
 }
 
-file_put_contents("newsletter.csv", $subscriberEmail."\n", FILE_APPEND | LOCK_EX);
+file_put_contents($NEWSLETTER_CSV, $subscriberEmail."\n", FILE_APPEND | LOCK_EX);
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
