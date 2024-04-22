d429c5d24957d732cc6408008bd0bbeff5b66bb7
Kevin Trotter <cskevint@gmail.com>
2024-04-22 09:57:51 -0700

    ignore empty emails

--- newsletter-capture.php
+++ newsletter-capture.php
@@ -2,21 +2,24 @@
 
 session_start();
 
-require_once("includes/utility.php");
+require_once ("includes/utility.php");
 
 $subscriberEmail = $_REQUEST['email'];
-$fromEmail = "k@doopr.com";
-$fromName = "Newsletter";
-$subject = "[HAFH] Newsletter";
-$messageBody = "<p>".$subscriberEmail."</p>";
 
-if (sendMail($fromEmail, $fromName, $subject, $messageBody)) {
-    $_SESSION['notice'] = "Your email address has been added.";
-} else {
-    $_SESSION['notice'] = 'There was an error. Please email us: <a href="mailto:selena@houndawayfromhome.com">selena@houndawayfromhome.com</a>';
+if (strlen(trim($subscriberEmail)) > 0) {
+    $fromEmail = "k@doopr.com";
+    $fromName = "Newsletter";
+    $subject = "[HAFH] Newsletter";
+    $messageBody = "<p>" . $subscriberEmail . "</p>";
+
+    if (sendMail($fromEmail, $fromName, $subject, $messageBody)) {
+        $_SESSION['notice'] = "Your email address has been added.";
+    } else {
+        $_SESSION['notice'] = 'There was an error. Please email us: <a href="mailto:selena@houndawayfromhome.com">selena@houndawayfromhome.com</a>';
+    }
+
+    file_put_contents($NEWSLETTER_CSV, $subscriberEmail . "\n", FILE_APPEND | LOCK_EX);
 }
-
-file_put_contents($NEWSLETTER_CSV, $subscriberEmail."\n", FILE_APPEND | LOCK_EX);
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
