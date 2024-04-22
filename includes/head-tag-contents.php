d8a3a24dd737a1aecae1da726c90ce8428a88340
Kevin Trotter <cskevint@gmail.com>
2024-04-22 16:16:23 -0700

    add og meta tags

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -1,9 +1,14 @@
+<?php
+$title = "Hound Away From Home".(isset($_GLOBALS["pageTitle"]) ? " - ".$_GLOBALS["pageTitle"] : "");
+?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description"
     content="Welcome to Hound Away from Home, where your beloved canine companion finds a safe haven for boarding and daycare, ensuring they receive the care, attention, and fun they deserve while you're away.">
 <meta name="author" content="Hound Away From Home, LLC">
-<title>Hound Away From Home<?= isset($_GLOBALS["pageTitle"]) ? " - ".$_GLOBALS["pageTitle"] : ""?></title>
+<title><?=$title?></title>
+<meta property="og:title" content="<?=$title?>">
+<meta property="og:image" content="/images/image-eli.jpg">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -20,13 +25,13 @@
 <meta name="theme-color" content="#ffffff">
 
 <?php if(str_contains($_SERVER["SERVER_NAME"],"houndawayfromhome.com")) { ?>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FRD4XKQCPT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-FRD4XKQCPT"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  gtag('config', 'G-FRD4XKQCPT');
-</script>
+    gtag('config', 'G-FRD4XKQCPT');
+  </script>
 <?php } ?>
