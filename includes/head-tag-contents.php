6efab5fd6aa90a0c70eafba29ab29846b5350663
Kevin Trotter <cskevint@gmail.com>
2024-04-29 18:16:12 -0700

    force update css

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -13,7 +13,7 @@ $title = "Hound Away From Home".(isset($_GLOBALS["pageTitle"]) ? " - ".$_GLOBALS
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<link rel="stylesheet" href="/styles/main.css" />
+<link rel="stylesheet" type="text/css" href="/styles/main.css?v=1" />
 
 <link rel="icon" href="favicon.ico">
 <link rel="apple-touch-icon" href="apple-touch-icon.png" sizes="180x180">
--- updatesite.php
+++ updatesite.php
@@ -1,9 +1,40 @@
 <?php
-echo exec("git pull --rebase");
+session_start();
+require_once ("includes/config.php");
 ?>
-<p>Redirecting to homepage in 5 seconds...</p>
-<script>
-window.setTimeout(function () {
-    window.location.href = "/";
-}, 5000);
-</script>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Under construction.</title>
+</head>
+
+<body>
+    <div class="container p-5">
+        <h1 class="text-center">Redirecting to homepage in 5 seconds...</h1>
+        <p class="text-center">
+            <?php
+                if ($GLOBALS['STAGE'] == "production") {
+                    echo exec("git pull --rebase");
+                }
+            ?>
+        </p>
+        <p class="text-center">
+            <a class="btn btn-primary btn-lg" href="/">Reload now</a>
+        </p>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script>
+    window.setTimeout(function() {
+        window.location.href = "/";
+    }, 5000);
+    </script>
+</body>
+
+</html>
