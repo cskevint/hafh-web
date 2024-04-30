860a3f615fc9d8be600f28681f982953dc42a33a
Kevin Trotter <cskevint@gmail.com>
2024-04-30 12:00:06 -0700

    update title

--- faqs.php
+++ faqs.php
@@ -17,7 +17,7 @@ $_GLOBALS["pageTitle"] = "FAQs";
     <section class="container-fluid px-0 bg-info">
         <div class="container p-lg-5 p-md-4 p-3 px-0">
             <div class="bg-light rounded-5 p-lg-5 p-md-4 p-3">
-                <h1 class="mb-ms-5 mb-3"><?=$_GLOBALS["pageTitle"]?></h1>
+                <h1 class="mb-ms-5 mb-3">Frequently Asked Questions</h1>
 
                 <?php 
                 include_once("lib/parsedown-1.7.4/Parsedown.php"); 
