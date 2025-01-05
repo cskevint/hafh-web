805c306e566a06e0411115b9af6d0cf6f3a9d190
Kevin Trotter <cskevint@gmail.com>
2025-01-05 10:09:47 -0800

    reorganized code

--- js/quiz/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { QuizData } from "./quiz";
-import QuizForm from "./QuizForm";
-
-function App() {
-  return <QuizForm quiz={QuizData} />;
-}
-
-export default App;
--- js/quiz/src/main.tsx
+++ js/quiz/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "vite/modulepreload-polyfill";
+import QuizForm from "./components/QuizForm";
+import { QuizData } from "./model/quiz";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QuizForm quiz={QuizData} />
   </StrictMode>
 );
