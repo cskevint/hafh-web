5a6d4076354ef0e379bc38891960a3a3560c587d
Kevin Trotter <cskevint@gmail.com>
2025-01-11 15:46:53 -0800

    dynamically read latest JS file

--- quiz.php
+++ quiz.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
-require_once ("redirect.php");
+require_once("redirect.php");
 $_GLOBALS["pageTitle"] = "Quiz";
 ?>
 <!doctype html>
 <html lang="en" class="bg-info">
 
 <head>
-    <?php include ("includes/head-tag-contents.php"); ?>
+    <?php include("includes/head-tag-contents.php"); ?>
 </head>
 
 <body>
 
-    <?php include ("includes/navigation.php"); ?>
+    <?php include("includes/navigation.php"); ?>
 
     <section class="container-fluid px-0 bg-info">
         <div class="container p-lg-5 p-md-4 p-3 px-0">
@@ -22,8 +22,9 @@ $_GLOBALS["pageTitle"] = "Quiz";
         </div>
     </section>
 
-    <?php include ("includes/footer.php"); ?>
-    <script type="module" crossorigin src="/js/quiz/dist/quiz.js"></script>
+    <?php include("includes/footer.php"); ?>
+    <script type="module" crossorigin src="/js/quiz/dist/assets/<?= scandir("./js/quiz/dist/assets")[2] ?>">
+    </script>
 </body>
 
 </html>
