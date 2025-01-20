3cf0eb78de40d02de4aeca6e7998c49199541f66
Kevin Trotter <cskevint@gmail.com>
2025-01-20 09:19:14 -0800

    minor fix

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -11,8 +11,8 @@ $title = "Hound Away From Home".(isset($_GLOBALS["pageTitle"]) ? " - ".$_GLOBALS
 <meta property="og:title" content="<?=$title?>">
 <meta property="og:image" content="/images/image-eli.jpg">
 
-<link rel="stylesheet" href="styles/sass/dist/hafh-bootstrap.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" type="text/css" href="/styles/sass/dist/hafh-bootstrap.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 <link rel="stylesheet" type="text/css" href="/styles/main.css?v=<?=time()?>" />
 
 <link rel="icon" href="favicon.ico">
