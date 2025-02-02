cd079bc2fc90e071faf965d09f9e096b6ae03ce2
Kevin Trotter <cskevint@gmail.com>
2025-02-02 15:51:54 -0800

    removed react quiz, added redirects for quiz and ebook

--- quiz.php
+++ quiz.php
@@ -1,30 +1,3 @@
 <?php
-session_start();
-require_once("redirect.php");
-$_GLOBALS["pageTitle"] = "Quiz";
+header("Location: /is-dog-boarding-right-for-me");
 ?>
-<!doctype html>
-<html lang="en" class="bg-info">
-
-<head>
-    <?php include("includes/head-tag-contents.php"); ?>
-</head>
-
-<body>
-
-    <?php include("includes/navigation.php"); ?>
-
-    <section class="container-fluid px-0 bg-info">
-        <div class="container p-lg-5 p-md-4 p-3 px-0">
-            <div class="p-lg-5 p-md-4 p-3">
-                <div id="root"></div>
-            </div>
-        </div>
-    </section>
-
-    <?php include("includes/footer.php"); ?>
-    <script type="module" crossorigin src="/js/quiz/dist/assets/<?= scandir("./js/quiz/dist/assets")[2] ?>">
-    </script>
-</body>
-
-</html>
