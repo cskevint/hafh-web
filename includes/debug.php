1fd880cfe5593f10fd2ec165026553bca4f774d2
Kevin Trotter <cskevint@gmail.com>
2025-02-08 11:46:30 -0800

    improvement for debug header

--- includes/debug.php
+++ includes/debug.php
@@ -1,9 +1,18 @@
 <?php if ($GLOBALS['DEBUG']) { ?>
-    <div class="container-fluid bg-white sticky-top redbox">
-        <div class="text-center d-block d-sm-none">xs</div>
-        <div class="text-center d-none d-sm-block d-md-none">sm</div>
-        <div class="text-center d-none d-md-block d-lg-none">md</div>
-        <div class="text-center d-none d-lg-block d-xl-none">lg</div>
-        <div class="text-center d-none d-xl-block">xl</div>
+    <div class="container-fluid bg-white sticky-top redbox text-center">
+        <span class="fw-bold d-inline-block d-sm-none">xs</span>
+        <span class="fw-bold d-none d-sm-inline-block d-md-none">sm</span>
+        <span class="fw-bold d-none d-md-inline-block d-lg-none">md</span>
+        <span class="fw-bold d-none d-lg-inline-block d-xl-none">lg</span>
+        <span class="fw-bold d-none d-xl-inline-block">xl</span>
+        <tt class="ps-5">width=<span id="width"></span></tt>
     </div>
+    <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function () {
+            window.addEventListener('resize', function () {
+                document.getElementById('width').innerHTML = window.innerWidth;
+            });
+            document.getElementById('width').innerHTML = window.innerWidth;
+        });
+    </script>
 <?php } ?>
