341db8243993a6d7eaff20c7722cfdce53303743
Kevin Trotter <cskevint@gmail.com>
2025-11-22 14:04:06 -0500

    Update contact.php redirect

--- contact.php
+++ contact.php
@@ -1,30 +1,4 @@
 <?php
-session_start();
-require_once "redirect.php";
-$_GLOBALS["pageTitle"] = "Contact Us";
+header("Location: /contactus");
+exit();
 ?>
-<!doctype html>
-<html lang="en" class="bg-info">
-
-<head>
-    <?php include "includes/head-tag-contents.php"; ?>
-</head>
-
-<body>
-
-    <?php include "includes/navigation.php"; ?>
-
-    <section class="container-fluid px-0 bg-info">
-        <div class="container p-lg-5 p-md-4 p-3 px-0">
-            <div class="bg-light rounded-5 p-lg-5 p-md-4 p-3">
-                <h1 class="mb-ms-5 mb-3"><?= $_GLOBALS["pageTitle"] ?></h1>
-                <p>Contact us at <a href="mailto:info@hafh.com">info@hafh.com</a></p>
-            </div>
-        </div>
-    </section>
-
-    <?php include "includes/footer.php"; ?>
-
-</body>
-
-</html>
