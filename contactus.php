094f75a6fc9c56f93531ff17947eca3f1636ede0
Kevin Trotter <cskevint@gmail.com>
2025-11-22 14:06:03 -0500

    Apply mobile styling globally

--- contactus.php
+++ contactus.php
@@ -17,7 +17,7 @@ $_GLOBALS["pageTitle"] = "Contact Us";
 
     <section class="container-fluid px-0 bg-info">
         <div class="container p-lg-5 p-md-4 p-3 px-0">
-            <div class="bg-light rounded-5 p-lg-5 p-md-4 p-3 contact-container">
+            <div class="bg-light rounded-5 p-lg-5 p-md-4 p-3">
                 <h1 class="mb-ms-5 mb-3"><?= $_GLOBALS["pageTitle"] ?></h1>
 
                 <?php
